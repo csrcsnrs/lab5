eb1914e2917e8d2ba57a21a849bb493769006abb
Cesar Cisneros <cecisner@ucsd.edu>
2014-02-12 17:12:26 -0800

    putting it together lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,7 +10,14 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+    $("a.nameListener").click(nameClick);
 }
+
+function nameClick(e){
+    e.preventDefault();
+    var currentName= $(this).text();
+    $(this).text(anagrammedName(currentName));
+};
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,20 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
+exports.addFriend = function(req, res) {
 	// Your code goes here
- }
+
+	
+
+	var newName = req.query.name;
+	var newDescription = req.query.description;
+
+	var newFriend = {
+		'name' : newName,
+		'description' : newDescription,
+		'imageURL' : "http://lorempixel.com/400/400/people"
+	};
+	
+        res.render('add', data);
+    console.log(newFriend);
+    data["friends"].push(newFriend);
+}
